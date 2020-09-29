88799e788b3da1c96374535a631f12d88dcf340a
michael onyeije <monyeije16@saintpeters.edu>
2020-09-29 17:38:07 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -16,7 +16,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  help = [{DON_G[name] DON_G[occupation] }, {JOELLE_VD[name] JOELLE_VD[occupation]},{PAT_M[name] PAT_M[occupation]},{KATE_G[name],KATE_G[occupation]},{BRUCE_G[name],BRUCE_G[occupation]}]
+  help = [[DON_G[name],DON_G[occupation] ], [JOELLE_VD[name],JOELLE_VD[occupation]],[PAT_M[name], PAT_M[occupation]],[KATE_G[name],KATE_G[occupation]],[BRUCE_G[name],BRUCE_G[occupation]]]
   return help
  
 end
